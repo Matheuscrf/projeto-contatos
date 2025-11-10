703426d1e3460214e2984712cfdc52376e78e109
Matheus Teixeira <149537799+Matheuscrf@users.noreply.github.com>
2025-11-10 10:46:48 -0300

    fix(ui): footer fixo no rodapé e espaço para conteúdo (app.component.scss)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import {
+  IonApp,
+  IonRouterOutlet,
+  IonFooter,
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonButton,
+  IonIcon,
+} from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  styleUrls: ['app.component.scss'],
+  imports: [RouterLink, IonApp, IonRouterOutlet, IonFooter, IonToolbar, IonGrid, IonRow, IonCol, IonButton, IonIcon],
 })
 export class AppComponent {
   constructor() {}
